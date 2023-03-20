afde2fa49c28ee970bdcfa7695a16213e52af0b0
Vivek Patel <vivek.p9737@gmail.com>
2023-03-20 15:12:24 -0700

    basemap point vector plotting

--- js/index.js
+++ js/index.js
@@ -1,92 +1,57 @@
-var geoJSONdata = {};
-const width = 750, height = 700
-var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
+var geoJSONdata = [];
+const width = 1200, height = 1200;
+var imageWidth = 1076;
+var imageHeight = 1144;
+var svg = d3.select("svg").attr("width", width).attr("height", height);
+var zoom;
+var imgLocation = "img/BaseMap.png"
 
 document.addEventListener("DOMContentLoaded", async () => {
     await modifyData();
-
-    // drawLineMap()
+    addEventListeners();
+    drawBaseImageMap();
     drawCircleMap()
-    // drawPolygonMap()
 });
 
 const getCSVdata = () =>
     Promise.all([
-        d3.json("data/basemap_points.geojson"),
+        d3.json("data/merged_points.json"),
     ]);
 
 async function modifyData() {
     const [raw_geoJSONdata] = await getCSVdata();
     geoJSONdata = raw_geoJSONdata
-    // const projection = d3.geoMercator()
-    // .center([0, 0])
-    // .scale(100)
-    // .translate([width / 2, height / 2]);
-    // geoJSONdata.features = geoJSONdata.features.filter((d) => {
-    //     console.log("hey")
-    //     if(typeof projection(d.geometry.coordinates)[0] != 'number' || typeof projection(d.geometry.coordinates)[1] != 'number') 
-    //         console.log(projection(d.geometry.coordinates)[0],projection(d.geometry.coordinates)[1])
-    // })
 }
 
-function drawPolygonMap() {
-    const projection = d3.geoMercator()
-        .scale(100)
-        .center([0, 0])
-        .translate([width / 2, height / 2]);
-
-    // Create a path generator
-    const path = d3.geoPath()
-        .projection(projection);
-
-    // Draw the polygons
-    svg.selectAll('path')
-        .data(geoJSONdata.features)
-        .enter()
-        .append('path')
-        .attr('d', d => path(d.geometry));
+function drawBaseImageMap() {
+    svg.append("image")
+        .attr("width", imageWidth)
+        .attr("height", imageHeight)
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("xlink:href", imgLocation)
 }
-
 
 function drawCircleMap() {
-    const projection = d3.geoMercator()
-        .center([0, 0])
-        .scale(100)
-        .translate([width / 2, height / 2]);
-
     svg.selectAll("circle")
-        .data(geoJSONdata.features)
+        .data(geoJSONdata)
         .enter()
         .append("circle")
-        .attr("cx", d => {
-            const aa = projection(d.geometry.coordinates)[0]
-            // console.log(aa)
-            return aa
-        })
-        .attr("cy", d => projection(d.geometry.coordinates)[1])
+        .attr("cx", function (d) { return d[0]; })
+        .attr("cy", function (d) { return d[1]; })
         .attr("r", 1)
-        .attr("fill", "red")
-        .attr("stroke", "black")
-        .attr("stroke-width", 0.2);
+        .attr("fill", "green")
+        // .style("opacity",0.5)
 
 }
 
+function handleZooming(e){
+    svg.select("image").attr("transform", e.transform)
+    svg.selectAll("circle")
+        .attr("transform", e.transform)
+}
 
-function drawLineMap(){
-    var projection = d3.geoMercator()
-    .center([0, 0])
-    .scale(100)
-    .translate([width / 2, height / 2]);
-
-  var path = d3.geoPath()
-  .projection(projection);
-  
-svg.selectAll("path")
-  .data(geoJSONdata.features)
-  .enter()
-  .append("path")
-  .attr("d", path);
-
+function addEventListeners(){
+    zoom = d3.zoom().on('zoom',handleZooming)
+    svg.call(zoom)
 }
--- verify.py
+++ verify.py
@@ -1,14 +1,51 @@
-import os,json
-filePath = "basemap_points.geojson"
-import geopandas as gpd
-import matplotlib.pyplot as plt
+import json
+from sklearn.cluster import KMeans
+import numpy as np
 
-# Load the GeoJSON data from file
-geojson_data = gpd.read_file(filePath)
+# Define the number of clusters you want to create
+n_clusters = 10000
 
-ax = geojson_data.plot(figsize=(10,10), alpha=0.5, edgecolor='k', aspect=1.0)
+# Load the x,y coordinate data from the JSON file
+print("Loading data...")
+with open('data/basemap_points.json', 'r') as f:
+    data = json.load(f)
 
-# Show the plot
-plt.show()
+# Convert the data to a numpy array
+data = np.array(data)
 
+# Create an instance of the k-means clustering algorithm
+print("Creating KMeans instance...")
+kmeans = KMeans(n_clusters=n_clusters)
 
+# Fit the algorithm to your data
+print("Fitting KMeans model...")
+kmeans.fit(data)
+
+# Get the cluster assignments for each point
+labels = kmeans.labels_
+
+# Print the number of points in each cluster
+print("Number of points in each cluster:")
+for i in range(n_clusters):
+    print(f"Cluster {i}: {np.sum(labels == i)} points")
+
+# Get the centroids of each cluster
+centroids = kmeans.cluster_centers_
+
+# Merge nearby points within each cluster by taking the average of their coordinates
+merged_points = []
+for i in range(n_clusters):
+    print(f"Merging cluster {i}...")
+    cluster_points = data[labels == i]
+    merged_point = np.mean(cluster_points, axis=0)
+    merged_points.append(merged_point.tolist())  # Convert numpy array to list
+
+# Print the number of merged points
+print(f"Number of merged points: {len(merged_points)}")
+
+# Write the merged points to a JSON file
+print("Writing output to file...")
+with open('data/merged_points.json', 'w') as f:
+    json.dump(merged_points, f)
+
+print("Done!")
