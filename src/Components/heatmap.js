2841562c8b8370b3416b4c505840d15b8793939f
Vivek Patel <vivek.p9737@gmail.com>
2023-04-04 15:47:01 -0700

    adds lasso with custom d3-lasso-v9 library

--- src/Components/heatmap.js
+++ src/Components/heatmap.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import * as d3 from "d3";
 
 import imgLocation from '../img/BaseMap.png';
 import mergedPointsFile from '../data/merged_points_10000.json';
 
 function Heatmap() {
+    let d3,d3Lasso;
+
     const [geoJSONdata, setGeoJSONdata] = useState(mergedPointsFile);
     const [participantsLocation, setParticipantsLocation] = useState([]);
 
@@ -13,29 +14,33 @@ function Heatmap() {
     let width = 800, height = 650;
     const imageWidth = 1076;
     const imageHeight = 1144;
-    
-    let svg, zoom;
+    const pointsRadius = 2;
+
+    let svg;
 
     useEffect(() => {
+        d3 = window.d3;
+
         svg = d3.select("svg").attr("width", width).attr("height", height);
         addEventListener();
         calculateSVGDimentions();
         // drawBaseImageMap();
         drawCircleMap();
+        initLasso();
     }, []);
 
     function drawBaseImageMap() {
         svg.append("image")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("x", 0)
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", 0)
             .attr("y", 0)
             .attr("xlink:href", imgLocation)
     }
 
     function drawCircleMap() {
-        if(!svg) return;
-        const imageScale  = d3.scaleLinear()
+        if (!svg) return;
+        const imageScale = d3.scaleLinear()
             .domain([0, imageWidth])
             .range([0, width]);
         const imageScaleY = d3.scaleLinear()
@@ -49,7 +54,7 @@ function Heatmap() {
             .attr("class", "map_points")
             .attr("cx", d => imageScale(d[0]))
             .attr("cy", d => imageScaleY(d[1]))
-            .attr("r", 1)
+            .attr("r", pointsRadius)
             .attr("fill", "green")
             .style("opacity", 0.5)
     }
@@ -63,18 +68,18 @@ function Heatmap() {
     function calculateSVGDimentions() {
         const windowWidth = window.innerWidth;
         const windowHeight = window.innerHeight;
-      
+
         width = windowWidth * 0.6;
         height = windowHeight - 100;
-      
+
         svg.attr("width", width).attr("height", height);
     }
 
     function addEventListener() {
-        zoom = d3.zoom()
-            .scaleExtent([1, 8])
-            .on("zoom", handleZooming);
-        svg.call(zoom);
+        // zoom = d3.zoom()
+        //     .scaleExtent([1, 8])
+        //     .on("zoom", handleZooming);
+        // svg.call(zoom);
 
         window.addEventListener("resize", () => {
             calculateSVGDimentions()
@@ -82,12 +87,46 @@ function Heatmap() {
         });
     }
 
+    function onLassoStart() {
+        d3Lasso.items().attr("r",pointsRadius).classed("not_possible", true).classed("selected", false);
+    }
+
+    function onLassoDraw() {
+    //     d3Lasso.possibleItems()
+    //       .classed("not_possible",false)
+    //       .classed("possible",true);
+    //   d3Lasso.notPossibleItems()
+    //       .classed("not_possible",true)
+    //       .classed("possible",false);
+    }
+
+    function onLassoEnd() {
+        d3Lasso.items()
+          .classed("not_possible",false)
+          .classed("possible",false);
+      d3Lasso.selectedItems()
+          .classed("selected",true)
+          .attr("r",5);
+    }
+
+    function initLasso() {
+        d3Lasso = d3.lasso()
+        .closePathDistance(305) 
+        .closePathSelect(true) 
+        .targetArea(svg)
+        .items(svg.selectAll("circle.map_points")) 
+        .on("start",onLassoStart) 
+        .on("draw",onLassoDraw) 
+        .on("end",onLassoEnd); 
+
+        svg.call(d3Lasso);
+    }
+
     return (
         <div className="flex items-center justify-center overflow-hidden">
             <svg></svg>
         </div>
     );
-  }
-  
-  export default Heatmap;
-  
+}
+
+export default Heatmap;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import Home from './Components/Home';
+import * as d3 from 'd3';
+import * as d3Lasso from "d3-lasso-v9";
+
+const d3Custom = Object.assign(d3,{lasso: d3Lasso.lasso})
+window.d3 = d3Custom;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
